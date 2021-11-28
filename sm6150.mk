6590a2371d5001641b53c7b5370f82255f4448d8
33bca <fabian.leutenegger@bluewin.ch>
2021-11-28 05:35:34 +0000

    sm6150-common: Build ParanoidDoze

--- sm6150.mk
+++ sm6150.mk
@@ -332,6 +332,10 @@ PRODUCT_PACKAGES += \
     SweetFrameworks \
     SweetSystemUI
 
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
